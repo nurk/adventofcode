9b090a66e54561c05a1c0293d1c874bf6efe72fc
Tom Briers <tbriers@telenet.be>
2021-11-29 09:09:45 +0100

    more messing with some BigDecimal formula parser

--- src/main/java/bigdecimal/FunctionAdapter.java
+++
@@ -1,8 +0,0 @@
-package bigdecimal;
-
-import java.math.BigDecimal;
-
-public abstract class FunctionAdapter {
-    public int getCount() { return 1; }
-    abstract BigDecimal eval(BigDecimal... args);
-}
