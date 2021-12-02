4a6477e6214d3044c865b3fdabd0d6a83d14c03d
Tom Briers <tbriers@telenet.be>
2021-12-02 07:22:12 +0100

    2021 day 2

---
+++ src/main/java/year2021/puzzle2/Puzzle2.java
@@ -0,0 +1,55 @@
+package year2021.puzzle2;
+
+import org.apache.commons.lang3.StringUtils;
+import util.Utils;
+
+import java.util.List;
+
+public class Puzzle2 {
+    public static void main(String[] args) throws Exception {
+        List<String> input = Utils.getInput("2021/input2.txt", (s) -> s);
+
+        partA(input);
+        partB(input);
+    }
+
+    private static void partA(List<String> input) {
+        long position = 0;
+        long depth = 0;
+
+        for (String s : input) {
+            String[] split = StringUtils.split(s, " ");
+
+            long value = Long.parseLong(split[1]);
+            switch (split[0]) {
+                case "forward" -> position += value;
+                case "up" -> depth -= value;
+                case "down" -> depth += value;
+            }
+        }
+
+        System.out.println(position * depth);
+    }
+
+    private static void partB(List<String> input) {
+        long position = 0;
+        long depth = 0;
+        long aim = 0;
+
+        for (String s : input) {
+            String[] split = StringUtils.split(s, " ");
+
+            long value = Long.parseLong(split[1]);
+            switch (split[0]) {
+                case "forward" -> {
+                    position += value;
+                    depth += aim * value;
+                }
+                case "up" -> aim -= value;
+                case "down" -> aim += value;
+            }
+        }
+
+        System.out.println(position * depth);
+    }
+}
