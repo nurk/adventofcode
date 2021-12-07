590137b72fb7725f8e59e377c25efa7b7b5ef805
Tom Briers <tom.briers@iriscare.brussels>
2021-12-07 07:50:10 +0100

    2021 day 7

---
+++ src/main/java/year2021/puzzle7/Puzzle7.java
@@ -0,0 +1,43 @@
+package year2021.puzzle7;
+
+import util.Utils;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.function.IntUnaryOperator;
+import java.util.stream.IntStream;
+
+public class Puzzle7 {
+    public static void main(String[] args) {
+        List<Integer> input = Utils.getInput("2021/input7.txt",
+                        line -> Arrays.stream(line.split(",")).map(Integer::valueOf))
+                .stream()
+                .flatMap(s -> s)
+                .toList();
+
+        Integer min = input.stream().min(Comparator.naturalOrder()).orElseThrow();
+        Integer max = input.stream().max(Comparator.naturalOrder()).orElseThrow();
+
+        System.out.println(minimumAlignmentCost(input, min, max, i -> i));
+        System.out.println(minimumAlignmentCost(input, min, max, i -> IntStream.rangeClosed(0, i).sum()));
+    }
+
+    private static Integer minimumAlignmentCost(List<Integer> input,
+                                                Integer min,
+                                                Integer max,
+                                                IntUnaryOperator fuelStepCostConverter) {
+        return IntStream.rangeClosed(min, max)
+                .map(position -> costOfPosition(input, position, fuelStepCostConverter))
+                .min().orElseThrow();
+    }
+
+    private static Integer costOfPosition(List<Integer> input,
+                                          Integer position,
+                                          IntUnaryOperator fuelStepCostConverter) {
+        return input.stream()
+                .map(i -> fuelStepCostConverter.applyAsInt(Math.abs(i - position)))
+                .mapToInt(Integer::intValue)
+                .sum();
+    }
+}
