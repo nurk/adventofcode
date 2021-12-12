25ee43afa4f86ac8564005a4a62c9ea7a00c57f0
Tom Briers <tbriers@telenet.be>
2021-12-12 08:57:58 +0100

    2021 day 12

--- src/main/java/year2021/puzzle12/Puzzle12.java
+++ src/main/java/year2021/puzzle12/Puzzle12.java
@@ -5,6 +5,7 @@ import util.Utils;
 
 import java.util.*;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Stream;
 
 public class Puzzle12 {
     public static void main(String[] args) {
@@ -12,14 +13,12 @@ public class Puzzle12 {
         Utils.getInput("2021/input12.txt", (s) -> s).stream()
                 .map(s -> s.split("-"))
                 .forEach(split -> {
-                    if (!caves.containsKey(split[0])) {
-                        caves.put(split[0], new ArrayList<>());
-                    }
-                    if (!caves.containsKey(split[1])) {
-                        caves.put(split[1], new ArrayList<>());
-                    }
-                    caves.get(split[0]).add(split[1]);
-                    caves.get(split[1]).add(split[0]);
+                    caves.merge(split[0],
+                            List.of(split[1]),
+                            (oldList, newList) -> Stream.of(oldList, newList).flatMap(Collection::stream).toList());
+                    caves.merge(split[1],
+                            List.of(split[0]),
+                            (oldList, newList) -> Stream.of(oldList, newList).flatMap(Collection::stream).toList());
                 });
 
         System.out.println(countPaths(new LinkedList<>(List.of("start")), caves, false));
