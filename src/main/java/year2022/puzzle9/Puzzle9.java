a9aabb301864d793fcfdfb9bb227b1ca1ec1d5e2
Tom Briers <tbriers@telenet.be>
2022-12-09 07:18:09 +0100

    2022 day 9

--- src/main/java/year2022/puzzle9/Puzzle9.java
+++ src/main/java/year2022/puzzle9/Puzzle9.java
@@ -6,7 +6,6 @@ import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
-import java.util.function.Function;
 
 public class Puzzle9 {
 
@@ -20,11 +19,11 @@ public class Puzzle9 {
         visited.add(positions.get(positions.size() - 1));
 
         for (String line : Utils.getInput("2022/input9.txt")) {
-            Move move = Move.valueOf(line.split(" ")[0]);
-            for (int i = 0; i < Integer.parseInt(line.split(" ")[1]); i++) {
-                positions.set(0, move.moveHead.apply(positions.get(0)));
+            String[] split = line.split(" ");
+            for (int i = 0; i < Integer.parseInt(split[1]); i++) {
+                positions.set(0, positions.get(0).moveAsHead(split[0]));
                 for (int j = 1; j < positions.size(); j++) {
-                    positions.set(j, moveTail(positions.get(j - 1), positions.get(j)));
+                    positions.set(j, positions.get(j - 1).moveTail(positions.get(j)));
                 }
                 visited.add(positions.get(positions.size() - 1));
             }
@@ -39,47 +38,44 @@ public class Puzzle9 {
             return Math.abs(x - other.x) > 1 && y != other.y || Math.abs(y - other.y) > 1 && x != other.x;
         }
 
-        public int getXMove(Position other) {
+        public Position moveAsHead(String m) {
+            return switch (m) {
+                case "L" -> new Position(x, y - 1);
+                case "R" -> new Position(x, y + 1);
+                case "U" -> new Position(x - 1, y);
+                case "D" -> new Position(x + 1, y);
+                default -> throw new IllegalArgumentException();
+            };
+        }
+
+        public Position moveTail(Position tail) {
+            int x = tail.x;
+            int y = tail.y;
+            if (this.needsDiagonalMove(tail)) {
+                return new Position(getXMove(tail), getYMove(tail));
+            }
+
+            if (Math.abs(this.x - tail.x) > 1) {
+                x = getXMove(tail);
+            }
+            if (Math.abs(this.y - tail.y) > 1) {
+                y = getYMove(tail);
+            }
+            return new Position(x, y);
+        }
+
+        private int getXMove(Position other) {
             if (x > other.x) {
                 return other.x + 1;
             }
             return other.x - 1;
         }
 
-        public int getYMove(Position other) {
+        private int getYMove(Position other) {
             if (y > other.y) {
                 return other.y + 1;
             }
             return other.y - 1;
         }
-    }
-
-    enum Move {
-        L(position -> new Position(position.x, position.y - 1)),
-        R(position -> new Position(position.x, position.y + 1)),
-        U(position -> new Position(position.x - 1, position.y)),
-        D(position -> new Position(position.x + 1, position.y));
-
-        private final Function<Position, Position> moveHead;
-
-        Move(Function<Position, Position> moveHead) {
-            this.moveHead = moveHead;
-        }
-    }
-
-    public static Position moveTail(Position head, Position tail) {
-        int x = tail.x;
-        int y = tail.y;
-        if (head.needsDiagonalMove(tail)) {
-            return new Position(head.getXMove(tail), head.getYMove(tail));
-        }
-
-        if (Math.abs(head.x - tail.x) > 1) {
-            x = head.getXMove(tail);
-        }
-        if (Math.abs(head.y - tail.y) > 1) {
-            y = head.getYMove(tail);
-        }
-        return new Position(x, y);
     }
 }
