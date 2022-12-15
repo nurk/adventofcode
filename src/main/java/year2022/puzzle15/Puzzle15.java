c76ceb24ece8a28ede06daff9e6adfc5d442ccf1
Tom Briers <tbriers@telenet.be>
2022-12-15 18:24:49 +0100

    2022 day 15 some cleanup

--- src/main/java/year2022/puzzle15/Puzzle15.java
+++ src/main/java/year2022/puzzle15/Puzzle15.java
@@ -10,29 +10,32 @@ import java.util.List;
 import java.util.stream.IntStream;
 
 public class Puzzle15 {
+
+    private static List<Sensor> sensors;
+
     public static void main(String[] args) {
         //6425133 -> correct partA
         //10996191429555 -> correct partB
-        List<Sensor> sensors = Utils.getInput("2022/input15.txt").stream()
+        sensors = Utils.getInput("2022/input15.txt").stream()
                 .map(Sensor::new)
                 .toList();
 
+        //Part A
         int minX = sensors.stream()
                 .map(s -> Math.min(s.sensorX, s.beaconX))
                 .min(Integer::compareTo)
-                .orElseThrow() - sensors.stream().map(s -> s.distance).max(Integer::compare).orElseThrow();
+                .orElseThrow() - sensors.stream().map(s -> s.manhattanDistance).max(Integer::compare).orElseThrow();
 
         int maxX = sensors.stream()
                 .map(s -> Math.max(s.sensorX, s.beaconX))
                 .max(Integer::compareTo)
-                .orElseThrow() + sensors.stream().map(s -> s.distance).max(Integer::compare).orElseThrow();
+                .orElseThrow() + sensors.stream().map(s -> s.manhattanDistance).max(Integer::compare).orElseThrow();
 
         System.out.println(IntStream.rangeClosed(minX, maxX).sequential()
-                .filter(x -> sensors.stream()
-                        .anyMatch(s -> s.isCoveredPartA(x, 2000000)))
+                .filter(x -> sensors.stream().anyMatch(s -> !s.canHaveBeacon(x, 2000000)))
                 .count());
 
-
+        //Part B
         int maxCoordinateValue = 4000000;
         boolean found = false;
         for (int y = 0; y <= maxCoordinateValue; y++) {
@@ -41,17 +44,20 @@ public class Puzzle15 {
                 int finalY = y;
 
                 Pair<Boolean, Integer> covered = sensors.stream()
-                        .map(s -> s.isCoveredPartB(finalX, finalY))
+                        .map(s -> s.isCoveredBySensorAndXMoveAmount(finalX, finalY))
                         .filter(Pair::getValue0)
                         .max(Comparator.comparing(Pair::getValue1))
                         .orElse(null);
-                if (covered != null && covered.getValue0() && covered.getValue1() != 0) {
+                if (covered != null && covered.getValue1() != 0) {
+                    //current position is covered by a sensor
+                    //move the x by the given value to skip a lot of checks
+                    //subtract 1 because loop will add 1 back
                     x = x + covered.getValue1() - 1;
                     continue;
                 }
 
                 if (sensors.stream()
-                        .noneMatch(s -> s.isCoveredPartB(finalX, finalY).getValue0())) {
+                        .noneMatch(s -> s.isCoveredBySensorAndXMoveAmount(finalX, finalY).getValue0())) {
                     System.out.println(BigInteger.valueOf(x)
                             .multiply(BigInteger.valueOf(4000000))
                             .add(BigInteger.valueOf(y)));
@@ -66,7 +72,7 @@ public class Puzzle15 {
     }
 
     static class Sensor {
-        int sensorX, sensorY, beaconX, beaconY, distance;
+        private final int sensorX, sensorY, beaconX, beaconY, manhattanDistance;
 
         public Sensor(String line) {
             String[] sensorPos = StringUtils.substringBetween(line, "Sensor at ", ":").split(", ");
@@ -77,20 +83,19 @@ public class Puzzle15 {
             beaconX = Integer.parseInt(StringUtils.substringAfter(beaconPos[0], "="));
             beaconY = Integer.parseInt(StringUtils.substringAfter(beaconPos[1], "="));
 
-            distance = Math.abs(sensorX - beaconX) + Math.abs(sensorY - beaconY);
+            manhattanDistance = Math.abs(sensorX - beaconX) + Math.abs(sensorY - beaconY);
         }
 
-        public boolean isCoveredPartA(int x, int y) {
-            if ((x == beaconX && y == beaconY)) {
-                return false;
+        public boolean canHaveBeacon(int x, int y) {
+            if (beaconX == x && beaconY == y) {
+                return true;
             }
-            int myDistance = Math.abs(sensorX - x) + Math.abs(sensorY - y);
-            return myDistance <= distance;
+            return Math.abs(sensorX - x) + Math.abs(sensorY - y) > manhattanDistance;
         }
 
-        public Pair<Boolean, Integer> isCoveredPartB(int x, int y) {
-            int myDistance = Math.abs(sensorX - x) + Math.abs(sensorY - y);
-            if (myDistance > distance) {
+        public Pair<Boolean, Integer> isCoveredBySensorAndXMoveAmount(int x, int y) {
+            int thisManhattanDistance = Math.abs(sensorX - x) + Math.abs(sensorY - y);
+            if (thisManhattanDistance > manhattanDistance) {
                 return Pair.with(false, 0);
             }
 
@@ -98,9 +103,9 @@ public class Puzzle15 {
             if (x < sensorX) {
                 moveXAmount = (sensorX - x) * 2 + 1;
             } else if (x > sensorX) {
-                moveXAmount = distance - myDistance + 1;
+                moveXAmount = manhattanDistance - thisManhattanDistance + 1;
             } else if (y == sensorY) {
-                moveXAmount = distance + 1;
+                moveXAmount = manhattanDistance + 1;
             }
 
             return Pair.with(true, moveXAmount);
