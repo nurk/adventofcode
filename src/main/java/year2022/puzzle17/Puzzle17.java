a732233f3ae32582e6109d08d62ae2fd0f56c3e6
Tom Briers <tbriers@telenet.be>
2022-12-17 09:33:24 +0100

    2022 day 17: partA, brute forcing B without loop detection. Will see if it ever finishes

--- src/main/java/year2022/puzzle17/Puzzle17.java
+++ src/main/java/year2022/puzzle17/Puzzle17.java
@@ -4,12 +4,12 @@ import util.Utils;
 
 import java.text.NumberFormat;
 import java.util.*;
-import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class Puzzle17 {
-    static List<Point> occupied = new ArrayList<>();
+    static SortedSet<Point> occupied = new TreeSet<>();
     static CircularList<String> gusts = new CircularList<>();
+    static CircularList<Shape> shapes;
     static int gustIndex = 0;
     static long shapeIndex = 0;
     static long lastCullingRow = 0;
@@ -19,7 +19,8 @@ public class Puzzle17 {
         System.out.println(formatter.format(1000000000000L));
         System.out.println(formatter.format(Long.MAX_VALUE));
         // partA 3149
-        CircularList<Shape> shapes = new CircularList<>(List.of(
+        // partB 1554593222610 too high <-- weird experiment with modulos
+        shapes = new CircularList<>(List.of(
                 new Shape(List.of(new Point(2, 0), new Point(3, 0), new Point(4, 0), new Point(5, 0))),
                 new Shape(List.of(new Point(3, 0), new Point(2, 1), new Point(3, 1), new Point(4, 1), new Point(3, 2))),
                 new Shape(List.of(new Point(2, 0), new Point(3, 0), new Point(4, 0), new Point(4, 1), new Point(4, 2))),
@@ -29,19 +30,16 @@ public class Puzzle17 {
 
         gusts = new CircularList<>(Utils.getInput("2022/input17.txt", s -> Arrays.stream(s.split("")).toList())
                 .get(0));
-
         IntStream.range(0, 7)
                 .forEach(i -> occupied.add(new Point(i, 0)));
 
-        // 1.000.000.000.000
         while (shapeIndex < 1000000000000L) {
-            //while (shapeIndex < 2022L) {
-            long maxY = occupied.stream().max(Comparator.comparingLong(o -> o.y)).orElseThrow().y;
-            Shape shape = shapes.getModulod(shapeIndex).clone(maxY + 4);
+            long maxY = occupied.last().y;
+            Shape shape = shapes.getModulo(shapeIndex).clone(maxY + 4);
             shapeIndex++;
             boolean atEnd = false;
             while (!atEnd) {
-                if (gusts.getModulod(gustIndex).equals("<")) {
+                if (gusts.getModulo(gustIndex).equals("<")) {
                     if (shape.canMoveLeft()) {
                         shape.moveLeft();
                     }
@@ -61,15 +59,36 @@ public class Puzzle17 {
             doCulling();
             if (shapeIndex % 100000L == 0) {
                 System.out.println("culled row " + formatter.format(lastCullingRow) + " shapeIndex: " + formatter.format(
-                        shapeIndex));
+                        shapeIndex) + " occupiedSize: " + formatter.format(occupied.size()));
+            }
+
+            if (shapeIndex == 2022L) {
+                System.out.println("PartA: " + occupied.stream().max(Comparator.comparing(o -> o.y)).orElseThrow().y);
             }
         }
-        System.out.println(occupied.stream().max(Comparator.comparing(o -> o.y)).orElseThrow().y);
+        System.out.println("PartB: " + occupied.stream().max(Comparator.comparing(o -> o.y)).orElseThrow().y);
     }
 
     private static void doCulling() {
         long maxY = occupied.stream().max(Comparator.comparing(o -> o.y)).orElseThrow().y;
         for (long i = maxY; i > lastCullingRow; i--) {
+            if ((shapeIndex % shapes.size() == 0
+                    && gustIndex % gusts.size() == 0
+                    && occupied.contains(new Point(0, i)))
+                    && (occupied.contains(new Point(1, i)))
+                    && (occupied.contains(new Point(2, i)))
+                    && (occupied.contains(new Point(3, i)))
+                    && (occupied.contains(new Point(4, i)))
+                    && (occupied.contains(new Point(5, i)))
+                    && (occupied.contains(new Point(6, i)))) {
+                // I do not seem to be hitting this.  Probably need to detect cycles
+                System.out.println("SPECIAL CULLLING ROW");
+                System.out.println("shapeIndex: " + shapeIndex);
+                System.out.println("culling row: " + i);
+                System.out.println("height at culling row " + occupied.stream()
+                        .max(Comparator.comparing(o -> o.y))
+                        .orElseThrow().y);
+            }
             if ((occupied.contains(new Point(0, i)) || occupied.contains(new Point(0, i - 1)))
                     && (occupied.contains(new Point(1, i)) || occupied.contains(new Point(1, i - 1)))
                     && (occupied.contains(new Point(2, i)) || occupied.contains(new Point(2, i - 1)))
@@ -81,11 +100,10 @@ public class Puzzle17 {
                 occupied.removeIf(p -> p.y < lastCullingRow);
                 break;
             }
-
         }
     }
 
-    static class Point {
+    static class Point implements Comparable<Point> {
         long x, y;
 
         public Point(long x, long y) {
@@ -106,14 +124,6 @@ public class Puzzle17 {
         }
 
         @Override
-        public String toString() {
-            return "Point{" +
-                    "x=" + x +
-                    ", y=" + y +
-                    '}';
-        }
-
-        @Override
         public boolean equals(Object o) {
             if (this == o) {
                 return true;
@@ -128,6 +138,18 @@ public class Puzzle17 {
                 return false;
             }
             return y == point.y;
+        }
+
+        @Override
+        public int hashCode() {
+            int result = (int) (x ^ (x >>> 32));
+            result = 31 * result + (int) (y ^ (y >>> 32));
+            return result;
+        }
+
+        @Override
+        public int compareTo(Point o) {
+            return this.y != o.y ? Long.compare(y, o.y) : Long.compare(x, o.x);
         }
     }
 
@@ -155,18 +177,6 @@ public class Puzzle17 {
                     .noneMatch(p -> occupied.contains(p));
         }
 
-        public void moveLeft() {
-            points.forEach(Point::moveLeft);
-        }
-
-        public void moveRight() {
-            points.forEach(Point::moveRight);
-        }
-
-        public void moveDown() {
-            points.forEach(Point::moveDown);
-        }
-
         public boolean canMoveRight() {
             Point maxX = points.stream()
                     .max(Comparator.comparingLong(o -> o.x))
@@ -186,27 +196,21 @@ public class Puzzle17 {
                     .noneMatch(p -> occupied.contains(p));
         }
 
-        @Override
-        public String toString() {
-            return "Shape{" +
-                    "points=" + points.stream().map(Point::toString).collect(Collectors.joining("\n")) +
-                    '}';
+        public void moveLeft() {
+            points.forEach(Point::moveLeft);
         }
+
+        public void moveRight() {
+            points.forEach(Point::moveRight);
+        }
+
+        public void moveDown() {
+            points.forEach(Point::moveDown);
+        }
+
     }
 
-/*    static void printOccupied() {
-        System.out.println(Utils.reverseRange(0,
-                        occupied.stream().max(Comparator.comparing(o -> o.y)).orElseThrow().y + 1)
-                .boxed()
-                .map(y -> IntStream.range(0, 7)
-                        .boxed()
-                        .map(x -> occupied.contains(new Point(x, y)) ? "#" : ".")
-                        .collect(Collectors.joining())
-                )
-                .collect(Collectors.joining("\n")));
-    }*/
-
-    private static class CircularList<T> extends ArrayList<T> {
+    static class CircularList<T> extends ArrayList<T> {
         public CircularList() {
         }
 
@@ -214,7 +218,7 @@ public class Puzzle17 {
             super(c);
         }
 
-        public T getModulod(long index) {
+        public T getModulo(long index) {
             return super.get((int) (index % size()));
         }
 
