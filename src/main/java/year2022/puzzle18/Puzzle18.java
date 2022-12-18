9249a5e42a6cfae7d4e1a85f48b221dc50104350
Tom Briers <tbriers@telenet.be>
2022-12-18 07:46:58 +0100

    2022 day 18 pat A

--- src/main/java/year2022/puzzle18/Puzzle18.java
+++ src/main/java/year2022/puzzle18/Puzzle18.java
@@ -10,11 +10,51 @@ public class Puzzle18 {
 
     public static void main(String[] args) {
         //Part A: 4192
+        //Part B: 3988 <-- too high.  Possibly there are encapsulate air pockets bigger than 1 block
         cubes = Utils.getInput("2022/input18.txt", Cube::new);
-        System.out.println(cubes.stream()
-                .map(Cube::exposedSides)
-                .reduce(0, Integer::sum));
+        Integer exposedSides = cubes.stream()
+                .map(Cube::getExposedSides)
+                .reduce(0, Integer::sum);
+        System.out.println(exposedSides);
 
+        int minX = cubes.stream()
+                .map(c -> c.x)
+                .min(Integer::compareTo)
+                .orElseThrow();
+        int maxX = cubes.stream()
+                .map(c -> c.x)
+                .max(Integer::compareTo)
+                .orElseThrow();
+        int minY = cubes.stream()
+                .map(c -> c.y)
+                .min(Integer::compareTo)
+                .orElseThrow();
+        int maxY = cubes.stream()
+                .map(c -> c.y)
+                .max(Integer::compareTo)
+                .orElseThrow();
+        int minZ = cubes.stream()
+                .map(c -> c.z)
+                .min(Integer::compareTo)
+                .orElseThrow();
+        int maxZ = cubes.stream()
+                .map(c -> c.z)
+                .max(Integer::compareTo)
+                .orElseThrow();
+
+        System.out.println();
+
+        int encapsulated = 0;
+        for (int x = minX; x <= maxX; x++) {
+            for (int y = minY; y <= maxY; y++) {
+                for (int z = minZ; z <= maxZ; z++) {
+                    if (new Cube(x, y, z).isEncapsulated()) {
+                        encapsulated++;
+                    }
+                }
+            }
+        }
+        System.out.println(exposedSides - encapsulated * 6);
     }
 
     static class Cube {
@@ -27,13 +67,36 @@ public class Puzzle18 {
             z = Integer.parseInt(split[2]);
         }
 
-        public int exposedSides() {
+        public Cube(int x, int y, int z) {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+
+        // only works when encapsulated air is a 1 by 1 by 1 cube
+        // does not work with maze like path that might not reach the outside
+        public boolean isEncapsulated() {
+            boolean b = !cubes.contains(this)
+                    && cubes.contains(new Cube(x - 1, y, z))
+                    && cubes.contains(new Cube(x + 1, y, z))
+                    && cubes.contains(new Cube(x, y - 1, z))
+                    && cubes.contains(new Cube(x, y + 1, z))
+                    && cubes.contains(new Cube(x, y, z - 1))
+                    && cubes.contains(new Cube(x, y, z + 1));
+            if (b) {
+                System.out.println(this + " is encapsulated");
+            }
+            return b;
+        }
+
+        public int getExposedSides() {
             int exposedSides = 6;
             for (Cube cube : cubes) {
                 if (cube.equals(this)) {
                     continue;
                 }
                 if (cube.touches(this)) {
+                    //System.out.println(this + " touches " + cube);
                     exposedSides--;
                 }
                 if (exposedSides == 0) {
