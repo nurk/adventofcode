8bbc201e2983e901de7f27d91ad7e7a7bacc0d75
Tom Briers <tbriers@telenet.be>
2022-12-19 05:02:10 +0100

    2022 day 17: messing in the margins

--- src/main/java/year2022/puzzle17/Puzzle17.java
+++ src/main/java/year2022/puzzle17/Puzzle17.java
@@ -1,44 +1,59 @@
 package year2022.puzzle17;
 
+import org.apache.commons.lang3.time.StopWatch;
 import util.Utils;
 
+import javax.annotation.Nonnull;
 import java.text.NumberFormat;
 import java.util.*;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class Puzzle17 {
-    static SortedSet<Point> occupied = new TreeSet<>();
-    static CircularList<String> gusts;
-    static CircularList<Shape> shapes;
+    static SortedSet<Point> occupied = new TreeSet<>(yComparator());
+
+    static List<String> gusts;
+    static List<Shape> shapes;
     static int gustIndex = 0;
-    static long shapeIndex = 0;
+    static int shapeIndex = 0;
     static long lastCullingRow = 0;
     static NumberFormat formatter = NumberFormat.getInstance(Locale.forLanguageTag("nl-BE"));
 
     public static void main(String[] args) {
         // partA 3149
         // partB 1554593222610 too high <-- weird experiment with modulos
-        shapes = new CircularList<>(List.of(
-                new Shape(List.of(new Point(2, 0), new Point(3, 0), new Point(4, 0), new Point(5, 0))),
-                new Shape(List.of(new Point(3, 0), new Point(2, 1), new Point(3, 1), new Point(4, 1), new Point(3, 2))),
-                new Shape(List.of(new Point(2, 0), new Point(3, 0), new Point(4, 0), new Point(4, 1), new Point(4, 2))),
-                new Shape(List.of(new Point(2, 0), new Point(2, 1), new Point(2, 2), new Point(2, 3))),
-                new Shape(List.of(new Point(2, 0), new Point(3, 0), new Point(2, 1), new Point(3, 1)))
-        ));
+        shapes = List.of(
+                new Shape(new TreeSet<>(Set.of(new Point(2, 0), new Point(3, 0), new Point(4, 0), new Point(5, 0)))),
+                new Shape(new TreeSet<>(Set.of(new Point(3, 0),
+                        new Point(2, 1),
+                        new Point(3, 1),
+                        new Point(4, 1),
+                        new Point(3, 2)))),
+                new Shape(new TreeSet<>(Set.of(new Point(2, 0),
+                        new Point(3, 0),
+                        new Point(4, 0),
+                        new Point(4, 1),
+                        new Point(4, 2)))),
+                new Shape(new TreeSet<>(Set.of(new Point(2, 0), new Point(2, 1), new Point(2, 2), new Point(2, 3)))),
+                new Shape(new TreeSet<>(Set.of(new Point(2, 0), new Point(3, 0), new Point(2, 1), new Point(3, 1))))
+        );
 
-        gusts = new CircularList<>(Utils.getInput("2022/input17.txt", s -> Arrays.stream(s.split("")).toList())
-                .get(0));
+        gusts = Utils.getInput("2022/input17.txt", s -> Arrays.stream(s.split("")).toList())
+                .get(0);
         IntStream.range(0, 7)
                 .forEach(i -> occupied.add(new Point(i, 0)));
 
+        StopWatch stopWatch = StopWatch.createStarted();
+        long counter = 0;
         //1.000.000.000.000L
-        while (shapeIndex < 1000000000000L) {
+        while (counter < 1000000000000L) {
+            counter++;
             long maxY = occupied.last().y;
-            Shape shape = shapes.getModulo(shapeIndex).clone(maxY + 4);
-            shapeIndex++;
+            Shape shape = shapes.get(shapeIndex).clone(maxY + 4);
+            shapeIndex = (shapeIndex + 1) % shapes.size();
             boolean atEnd = false;
             while (!atEnd) {
-                if (gusts.getModulo(gustIndex).equals("<")) {
+                if (gusts.get(gustIndex).equals("<")) {
                     if (shape.canMoveLeft()) {
                         shape.moveLeft();
                     }
@@ -47,7 +62,7 @@ public class Puzzle17 {
                         shape.moveRight();
                     }
                 }
-                gustIndex++;
+                gustIndex = (gustIndex + 1) % gusts.size();
                 if (shape.canMoveDown()) {
                     shape.moveDown();
                 } else {
@@ -56,12 +71,13 @@ public class Puzzle17 {
                 }
             }
             doCulling();
-            if (shapeIndex % 100000L == 0) {
-                System.out.println("culled row " + formatter.format(lastCullingRow) + " shapeIndex: " + formatter.format(
-                        shapeIndex) + " occupiedSize: " + formatter.format(occupied.size()));
+            if (counter % 1000000L == 0) {
+                stopWatch.split();
+                System.out.println(stopWatch.formatSplitTime() + " culled row " + formatter.format(lastCullingRow) + " counter: " + formatter.format(
+                        counter) + " occupiedSize: " + formatter.format(occupied.size()));
             }
 
-            if (shapeIndex == 2022L) {
+            if (counter == 2022L) {
                 System.out.println("PartA: " + occupied.last().y);
             }
         }
@@ -70,7 +86,7 @@ public class Puzzle17 {
 
     private static void doCulling() {
         long maxY = occupied.last().y;
-        for (long i = maxY; i > lastCullingRow; i--) {
+        for (long i = maxY; i > lastCullingRow + 2; i--) {
             if ((occupied.contains(new Point(0, i)) || occupied.contains(new Point(0, i - 1)))
                     && (occupied.contains(new Point(1, i)) || occupied.contains(new Point(1, i - 1)))
                     && (occupied.contains(new Point(2, i)) || occupied.contains(new Point(2, i - 1)))
@@ -110,10 +126,6 @@ public class Puzzle17 {
             if (this == o) {
                 return true;
             }
-            if (o == null || getClass() != o.getClass()) {
-                return false;
-            }
-
             Point point = (Point) o;
 
             if (x != point.x) {
@@ -130,26 +142,26 @@ public class Puzzle17 {
         }
 
         @Override
-        public int compareTo(Point o) {
-            return this.y != o.y ? Long.compare(y, o.y) : Long.compare(x, o.x);
+        public int compareTo(@Nonnull Point o) {
+            return xComparator().compare(this, o);
         }
     }
 
     static class Shape {
-        List<Point> points;
+        SortedSet<Point> points;
 
-        public Shape(List<Point> points) {
+        public Shape(SortedSet<Point> points) {
             this.points = points;
         }
 
         public Shape clone(long yIncrement) {
-            return new Shape(points.stream().map(p -> new Point(p.x, p.y + yIncrement)).toList());
+            return new Shape(points.stream()
+                    .map(p -> new Point(p.x, p.y + yIncrement))
+                    .collect(Collectors.toCollection(TreeSet::new)));
         }
 
         public boolean canMoveLeft() {
-            Point minX = points.stream()
-                    .min(Comparator.comparingLong(o -> o.x))
-                    .orElseThrow();
+            Point minX = points.first();
 
             if (minX.x - 1 < 0) {
                 return false;
@@ -160,9 +172,7 @@ public class Puzzle17 {
         }
 
         public boolean canMoveRight() {
-            Point maxX = points.stream()
-                    .max(Comparator.comparingLong(o -> o.x))
-                    .orElseThrow();
+            Point maxX = points.last();
 
             if (maxX.x + 1 > 6) {
                 return false;
@@ -192,13 +202,12 @@ public class Puzzle17 {
 
     }
 
-    static class CircularList<T> extends ArrayList<T> {
-        public CircularList(Collection<? extends T> c) {
-            super(c);
-        }
+    private static Comparator<Point> xComparator() {
+        return (o1, o2) -> o1.x != o2.x ? Long.compare(o1.x,
+                o2.x) : Long.compare(o1.y, o2.y);
+    }
 
-        public T getModulo(long index) {
-            return super.get((int) (index % size()));
-        }
+    private static Comparator<Point> yComparator() {
+        return (o1, o2) -> o1.y != o2.y ? Long.compare(o1.y, o2.y) : Long.compare(o1.x, o2.x);
     }
 }
