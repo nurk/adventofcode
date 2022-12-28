2dbb2ad3d846f1e1a8bc81bf86ca635149f2cc1a
Tom Briers <tbriers@telenet.be>
2022-12-28 04:10:28 +0100

    2016 day 14

---
+++ src/main/java/year2016/puzzle14/Puzzle14.java
@@ -0,0 +1,59 @@
+package year2016.puzzle14;
+
+import org.apache.commons.codec.digest.DigestUtils;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Puzzle14 {
+    static String salt = "ihaygndm";
+    static Pattern threeOfTheSame = Pattern.compile(".*?([a-z\\d])\\1\\1.*", Pattern.CASE_INSENSITIVE);
+    static boolean isPartA = false;
+    static Map<String, String> hashes = new HashMap<>();
+
+
+    // Part A: 15035
+    // Part B: 19968
+    public static void main(String[] args) {
+        int keysFound = 0;
+        int index = 0;
+        while (keysFound != 64) {
+            if (isKey(index)) {
+                keysFound++;
+            }
+            index++;
+        }
+        System.out.println(--index);
+    }
+
+    static boolean isKey(int index) {
+        String key = getKey(index);
+        Matcher matcher = threeOfTheSame.matcher(key);
+        if (matcher.matches()) {
+            String letter = matcher.group(1);
+            for (int i = index + 1; i <= index + 1000; i++) {
+                if (getKey(i).contains(letter + letter + letter + letter + letter)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    static String getKey(int index) {
+        String key = salt + index;
+        return hashes.computeIfAbsent(key, k -> {
+            String hash = DigestUtils.md5Hex(k);
+            if (isPartA) {
+                return hash;
+            } else {
+                for (int i = 0; i < 2016; i++) {
+                    hash = DigestUtils.md5Hex(hash);
+                }
+                return hash;
+            }
+        });
+    }
+}
