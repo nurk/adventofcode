07ad7beb43f82105b6c590873b6e8142038425cd
Tom Briers <tbriers@telenet.be>
2023-12-25 18:09:46 +0100

    2023 puzzle4 part a

---
+++ src/main/java/year2023/puzzle4/Card.java
@@ -0,0 +1,74 @@
+package year2023.puzzle4;
+
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Card {
+
+    private final Integer cardId;
+    private final List<Integer> winningNumbers;
+    private final List<Integer> scratchedNumbers;
+
+    public Card(String line) {
+        line = StringUtils.replace(line, "  ", " ");
+        cardId = Integer.parseInt(StringUtils.substringAfter(StringUtils.substringBefore(line, ":"), "Card ").trim());
+        line = StringUtils.substringAfter(line, ": ");
+        String[] split = line.split(" \\| ");
+
+        winningNumbers = Stream.of(split[0].split(" "))
+                .map(Integer::parseInt)
+                .toList();
+
+        scratchedNumbers = Stream.of(split[1].split(" "))
+                .map(Integer::parseInt)
+                .toList();
+        System.out.println(cardId);
+    }
+
+    public int getScore() {
+        int score = 0;
+        for (Integer scratchedNumber : scratchedNumbers) {
+            if (winningNumbers.contains(scratchedNumber)) {
+                if (score == 0) {
+                    score = 1;
+                } else {
+                    score = score * 2;
+                }
+            }
+        }
+        return score;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        Card card = (Card) o;
+
+        return cardId.equals(card.cardId);
+    }
+
+    @Override
+    public int hashCode() {
+        return cardId.hashCode();
+    }
+
+    @Override
+    public String toString() {
+        return "Card{" +
+                "cardId=" + cardId + ", " +
+                "winningNumbers=" + winningNumbers.stream().map(Object::toString).collect(Collectors.joining(",")) +
+                ", scratchedNumbers=" + scratchedNumbers.stream()
+                .map(Object::toString)
+                .collect(Collectors.joining(",")) +
+                '}';
+    }
+}
---
+++ src/main/java/year2023/puzzle4/Puzzle4.java
@@ -0,0 +1,19 @@
+package year2023.puzzle4;
+
+import util.Utils;
+
+import java.util.List;
+
+public class Puzzle4 {
+
+    public static void main(String[] args) {
+        List<Card> cards = Utils.getInput("2023/input4.txt").stream()
+                .map(Card::new)
+                .toList();
+
+        System.out.println("Part A: " + cards.stream()
+                .map(Card::getScore)
+                .mapToInt(value -> value)
+                .sum());
+    }
+}
