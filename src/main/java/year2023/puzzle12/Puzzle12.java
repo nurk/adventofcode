c32fb2ff4e6197a32f627b09da7c4257655e987c
Tom Briers <tbriers@telenet.be>
2025-12-22 08:04:57 +0100

    2023 puzzle 12 Not really getting part 2

--- src/main/java/year2023/puzzle12/Puzzle12.java
+++ src/main/java/year2023/puzzle12/Puzzle12.java
@@ -13,19 +13,6 @@ public class Puzzle12 {
         List<Spring> springs = Utils.getInput("2023/input12.txt", Spring::new);
         List<SpringAI> aiSprings = Utils.getInput("2023/input12.txt", SpringAI::new);
 
-        /**
-         * ???.### 1,1,3 - 1 arrangement
-         * .??..??...?##. 1,1,3 - 16384 arrangements
-         * ?#?#?#?#?#?#?#? 1,3,1,6 - 1 arrangement
-         * ????.#...#... 4,1,1 - 16 arrangements
-         * ????.######..#####. 1,6,5 - 2500 arrangements
-         * ?###???????? 3,2,1 - 506250 arrangements
-         *                      759375
-         */
-
-//        System.out.println(springs.get(5).solvePartB());
-//        System.out.println("-----------------------------------");
-        //springs.getFirst().solvePartB();
         System.out.println("Part A: " + springs.stream()
                 .map(Spring::solvePartA)
                 .mapToLong(value -> value)
